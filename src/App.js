9a714621309f8436d704a933acef00e2e95d4aed
Jasim Uddin <jasimbdpro@gmail.com>
2024-06-21 19:29:28 +0600

    nw

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Link, Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import { Link, Route, HashRouter, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './components/Home/Home';
 import NoMatch from './components/NoMatch/NoMatch';
@@ -8,7 +8,7 @@ function App() {
 
   return (
 
-    <Router>
+    <HashRouter>
       <Link to='/'>Go Home</Link>
       <Routes>
         <Route exact path='/' element={<Home></Home>}></Route>
@@ -16,7 +16,7 @@ function App() {
         <Route path='/friend/:friendId' element={<FriendDetail></FriendDetail>}></Route>
         <Route exact path='*' element={<NoMatch></NoMatch>}></Route>
       </Routes>
-    </Router>
+    </HashRouter>
   );
 }
 
